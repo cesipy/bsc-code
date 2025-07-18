79fef9b73eef2709b425c279c396208c1816d4d3
Cedric Sillaber <cedric.sillaber@gmail.com>
2025-07-18 21:30:43 +0200

    custom dataset implementation

---
+++ README.md
@@ -0,0 +1,5 @@
+# ViLBERT implementation
+
+## TODO: 
+- [ ] Tokenizer for text dependency injected
+- [ ] caching , [mmap](https://github.com/DACUS1995/pytorch-mmap-dataset/blob/main/pytorch_mmap_dataset/dataset.py)
--- src/config.py
+++ src/config.py
@@ -3,3 +3,11 @@ VOCAB_SIZE    = 30522
 NUM_HIDDEN_LAYERS = 12
 NUM_ATTENTION_HEADS = 12
 DROPOUT_PROB        = 0.1
+
+
+# data specific
+IMG_SIZE = (224, 224)
+PREPROCESSED_PATH = "res/preprocessed.pkl"      # not yet used, used to store precomputed datasets (in tensor form)
+TRAIN_TEST_RATIO = 0.8
+
+BATCH_SIZE = 32
